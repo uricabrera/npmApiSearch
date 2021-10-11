bc20618501ef46b7be9bcf19414641078b09137d
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-11 15:40:48 -0300

    finished RepositoriesList component

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useActions } from "../hooks/useActions";
 
 
 const RepositoriesList: React.FC = () => {
     const [term,setTerm] = useState("");
+    const { searchRepositories } = useActions();
+    const {data,error,loading} = useSelector((state: any) => state.repositories)
+    console.log(data);
 
     const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
+        searchRepositories(term);
     }
 
     return (<div>
@@ -14,6 +19,15 @@ const RepositoriesList: React.FC = () => {
             <input value={term} onChange={e => setTerm(e.target.value)}/>
             <button>Search</button>
         </form>
+        <div>
+            <ul>
+                {
+                    data.map((item: any) => {
+                        return <li>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
     </div>)
 }
 
