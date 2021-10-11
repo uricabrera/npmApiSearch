0d192af149e83004d07353759f274babcc75b088
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-11 14:34:56 -0300

    added reducer for redux store

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,40 @@
+interface RepositoriesState {
+    loading: boolean;
+    error: string | null;
+    data: string[];
+}
+
+interface SearchRepositoriesAction {
+    type: "search_repositories";
+}
+
+interface SearchRepositoriesSuccessAction {
+    type: "search_repositories_success";
+    payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+    type: "search_repositories_error";
+    payload: string;
+}
+
+enum ActionType {
+    SEARCH_REPOSITORIES = "search_repositories",
+    SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
+    SEARCH_REPOSITORIES_ERROR = "search_repositories_error"
+}
+
+const reducer = (state: RepositoriesState,action: SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction): RepositoriesState => {
+    switch(action.type){
+        case ActionType.SEARCH_REPOSITORIES:
+            return { loading: true, error: null, data: []}
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+            return { loading: false, error: null, data: action.payload}
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
+            return { loading: false, error: action.payload, data: []}
+        default:
+            return state;
+    }
+}
+
+export default reducer;
